f12677d8e90b6e30a1d9fc0e28ad4eb736cef26c
willzkn <grekelzkn@gmail.com>
2025-11-26 12:02:36 -0500

    Se adiciona metricas de vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
 import './index.css';
@@ -7,6 +8,7 @@ import './index.css';
 function App() {
   return (
     <Router>
+      <SpeedInsights />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetails />} />
